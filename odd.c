9cbc6cb43cbff9d892ee888113f1a243cfd2376e
Noorealam786 <shaikhnoorealam76@gmail.com>
2025-10-06 17:03:46 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,24 @@
+//class:civil FE
+//Name:Shaikh Noorealam 
+//UIN:251C006
+//Divion:B
+
+#include<stdio.h>
+int main()
+{
+    int start,end,sum=0;
+    printf("the staring number:");
+    scanf("%d" ,&start);
+    printf("the ending number:");
+    scanf("%d" ,&end);
+    for(int i=start; i<=end;i++)
+    {
+        if(i%2!=0)
+        {
+            sum+=i;
+        }
+    }
+    printf("the sum, of all odd number between %d and %d\n",start,end,sum);
+
+    return 0;
+}
