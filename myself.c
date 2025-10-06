9e1582da9b2d536c8a638c3171f1f27b993fd19a
Noorealam786 <shaikhnoorealam76@gmail.com>
2025-10-06 16:26:30 +0530

    Add files via upload

---
+++ myself.c
@@ -0,0 +1,16 @@
+//class:civil FE
+//Name:Shaikh Noorealam 
+//UIN:251C006
+//Divion:B
+
+#include<stdio.h>
+int main()
+{
+    printf("Name\t\t :Shaikh Noorealam\n");
+    printf("Age\t\t :17\n");
+    printf("Class\t\t :Civil FE\n");
+    printf("Divion\t\t :B\n");
+    printf("UIN\t\t :251C006\n");
+
+    return 0;
+}
